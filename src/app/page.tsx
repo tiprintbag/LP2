7b6a2a18a97eb8cc421ed73568f3428a460f50e7
Pedro Henrique Levorato <pedro.levorato@weisul.com.br>
2025-11-13 14:50:39 -0300

    Ajustar logo Printbag: menor e no canto superior esquerdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ import { getAssetPath } from '@/utils/paths'
 export default function Home() {
   return (
     <>
-      {/* Logo Printbag no topo */}
-      <section className="w-full bg-white py-4 sm:py-6 lg:py-8">
+      {/* Logo Printbag no topo esquerdo */}
+      <section className="w-full bg-white py-2 sm:py-3 lg:py-4">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-start items-center">
             <img
               src={getAssetPath('/images/printbag.png')}
               alt="Printbag Logo"
-              className="h-12 sm:h-16 md:h-20 lg:h-24 xl:h-28 2xl:h-32 w-auto object-contain"
+              className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto object-contain"
               loading="eager"
             />
           </div>
